fa51e13056d1b9cae0331c8d8877d14096882882
benjaminmgross <benjaminmgross@yahoo.com>
2014-03-20 03:00:16 -0400

    tested the adjusted r-squared linear programming method against sectors & style-cap as a second stage asset class determination and added a sample file to run the algorithm against

--- asset_class/asset_class.py
+++ asset_class/asset_class.py
@@ -25,74 +25,86 @@ def r_squared_adj(portfolio_prices, asset_prices, weights):
     sse = ((estimate - portfolio_returns)**2).sum()
     sst = ((portfolio_returns - portfolio_returns.mean())**2).sum()
     rsq = 1 - sse/sst
-    p, n = weights.shape[0], asset_returns.shape[0]
+    p, n = weights.shape[0], asset_returns.dropna().shape[0]
     return rsq - (1 - rsq)*(float(p)/(n - p - 1))
     
-def r_squared(portfolio_prices, asset_prices, weights):
+def r_squared(portfolio_prices, asset_prices):
     """
-    The unadjusted R-Squared
+    Univariate R-Squared
     """
     asset_returns = asset_prices.pct_change()
     portfolio_returns = portfolio_prices.pct_change()
-    estimate = numpy.dot(asset_returns, weights)
-    sse = ((estimate - portfolio_returns)**2).sum()
+    sse = ((asset_returns - portfolio_returns)**2).sum()
     sst = ((portfolio_returns - portfolio_returns.mean())**2).sum()
     return 1 - sse/sst
 
-def load_asset_classes():
+def load_asset_classes(asset_series):
     """
     Load the different prices that can be determined to find the "Broad Asset Class"
     into a :class:`pandas.Panel` and then pickle the data into ``../data/indexes``.
     """
+    ac_dict = {'VTSMX':'US Equity', 'VBMFX':'Fixed Income', 'VGTSX':'Intl Equity',
+               'IYR':'Alternative', 'GLD':'Alternative', 'GSG':'Alternative'}
+        
     asset_classes = ['US Equity', 'Fixed Income', 'Intl Equity', 'Alternative']
     tickers = ['VTSMX', 'VBMFX', 'VGTSX', 'PFF','IYR','GLD','GSG']
-    return None
+    data = web.DataReader(tickers, 'yahoo')['Adj Close']
+    rsq_d = {}
+    for ticker in data.columns:
+        ind = asset_series.index & data[ticker].index
+        rsq_d[ticker] = r_squared(asset_series[ind], data[ticker][ind])
+    rsq = pandas.Series(rsq_d)
+    return ac_dict[rsq.argmax()]
 
-def best_fitting_weights(asset_prices, asset_class_prices):
-	"""
-	:ARGS:
-    
-	    asset_prices: m x 1 :class:`pandas.TimeSeries` of asset_prices
+def best_fitting_weights(asset_prices, ac_prices):
+    """
+    :ARGS:
 
-        ac_prices: m x n :class:`pandas.DataFrame` asset_class prices
+        asset_prices: m x 1 :class:`pandas.TimeSeries` of asset_prices
 
-	:RETURNS:
-	
-	    :class:`pandas.TimeSeries` of nonnegative weights for each asset
-	    such that the r_squared from the regression of Y ~ Xw + e is maximized
+        ac_prices: m x n :class:`pandas.DataFrame` asset class ("ac") prices
 
-	"""
-	def _r_squared(weights):
-		"""
-		The Adjusted R-Squared that incorporates the number of independent variates
-		"""
-		estimate = asset_returns.dot(weights)
-		sse = ((estimate - portfolio_returns)**2).sum()
-		sst = ((portfolio_returns - portfolio_returns.mean())**2).sum()
-		return 1 - sse/sst
+    :RETURNS:
 
-	def _obj_fun(weights):
-		"""
-		To maximize the r_squared, minimize the negative of r_squared
-		"""	 
-		return - _r_squared(weights)
+        :class:`pandas.TimeSeries` of nonnegative weights for each asset
+        such that the r_squared from the regression of Y ~ Xw + e is maximized
 
+    """
 
-	
+    def _r_squared_adj(weights):
+        """
+        The Adjusted R-Squared that incorporates the number of independent variates
+        using the `Formula Found of Wikipedia
+        <http://en.wikipedia.org/wiki/Coefficient_of_determination#Adjusted_R2>_`
+        """
 
-	num_assets = asset_returns.shape[1]
-	guess = numpy.zeros(num_assets,)
-	#sum_to_one = lambda x: numpy.dot(numpy.tile(x, num_assets,),
+        estimate = numpy.dot(asset_returns, weights)
+        sse = ((estimate - portfolio_returns)**2).sum()
+        sst = ((portfolio_returns - portfolio_returns.mean())**2).sum()
+        rsq = 1 - sse/sst
+        p, n = weights.shape[0], asset_returns.dropna().shape[0]
+        return rsq - (1 - rsq)*(float(p)/(n - p - 1))
+
+    def _obj_fun(weights):
+        """
+        To maximize the r_squared_adj minimize the negative of r_squared
+        """
+        return -_r_squared_adj(weights)
+
+    asset_returns = ac_prices.pct_change()
+    portfolio_returns = asset_prices.pct_change()
+    num_assets = asset_returns.shape[1]
+    guess = numpy.zeros(num_assets,)
+    #sum_to_one = lambda x: numpy.dot(numpy.tile(x, num_assets,),
     #numpy.ones(num_assets,)) - 1
 
-	#ensure the boundaries of the function are (0, 1)
-	ge_zero = [(0,1) for i in numpy.arange(num_assets)]
+    #ensure the boundaries of the function are (0, 1)
+    ge_zero = [(0,1) for i in numpy.arange(num_assets)]
 
-	#optimize to maximize r-squared, using the 'TNC' method (that uses the boundary
+    #optimize to maximize r-squared, using the 'TNC' method (that uses the boundary
     #functionality)
-	opt = sopt.minimize(_obj_fun, x0 = guess, method = 'TNC', bounds = ge_zero)
+    opt = sopt.minimize(_obj_fun, x0 = guess, method = 'TNC', bounds = ge_zero)
 
-	normed = opt.x*(1./numpy.sum(opt.x))
+    normed = opt.x*(1./numpy.sum(opt.x))
 
-	return pandas.TimeSeries(normed, index = asset_returns.columns)
-    
+    return pandas.TimeSeries(normed, index = asset_returns.columns)
